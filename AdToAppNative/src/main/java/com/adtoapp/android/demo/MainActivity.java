078c364072782349300ec2e154f0e444f389ed36
Pavel Iohin <pavel@iohin.ru>
2016-02-09 16:45:59 +0500

    Native ads demo

---
+++ AdToAppNative/src/main/java/com/adtoapp/android/demo/MainActivity.java
@@ -0,0 +1,241 @@
+package com.adtoapp.android.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.appintop.init.AdToApp;
+import com.appintop.nativeads.AdToAppNative;
+import com.appintop.nativeads.NativeAd;
+import com.appintop.nativeads.OnNativeAdsAvailabilityListener;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    static final String TAG = "AdToAppSDKDemo";
+    static final int ADS_COUNT = 5;
+    static final int MAX_ATTEMPT_COUNT = 10;
+
+    List<String> items;
+    List<NativeAd> ads;
+    ListView listView;
+    MyAdapter listAdapter;
+    int attemptCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+
+        items = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            items.add("Item " + i);
+        }
+        ads = new ArrayList<>();
+
+        AdToApp.setLogging(true);
+        AdToApp.initializeSDK(this,
+                "710860d4-6c77-45ab-91eb-062955745997:42e13e6f-33d2-4656-98ae-456324801e51",
+                AdToApp.MASK_NATIVE);
+        AdToApp.setNativeAdsAvailabilityListener(new OnNativeAdsAvailabilityListener() {
+            @Override
+            public void onAvailable() {
+                loadAds();
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d(TAG, "NativeAds onError: " + error);
+                Toast toast = Toast.makeText(MainActivity.this,
+                        String.format("NativeAds onError:  %s", error),
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        listAdapter = new MyAdapter();
+        listView.setAdapter(listAdapter);
+    }
+
+    void loadAds() {
+        if (ads.size() >= ADS_COUNT) {
+            return;
+        }
+
+        AdToApp.loadNativeAd(this, new AdToAppNative.AdLoadListener() {
+            @Override
+            public void onLoadAd(final NativeAd ad, int providerId) {
+                // Use object ad for display Native advertising
+                ads.add(ad);
+
+                listAdapter.notifyDataSetChanged();
+
+                // Setup expired ad event listener (for RTB providers)
+                ad.setOnAdExpiredListener(new NativeAd.OnAdExpiredListener() {
+                    @Override
+                    public void onAdExpired(NativeAd nativeAd) {
+                        ads.remove(nativeAd);
+                        listAdapter.notifyDataSetChanged();
+                        loadAds();
+                    }
+                });
+
+                loadAds();
+            }
+
+            @Override
+            public AdToAppNative.Action onFailLoadAd(String error, int providerId) {
+                // Failed to load ad one of the providers in the queue
+                // By default try to load ad of the next provider
+                return AdToAppNative.Action.TRY_NEXT_PROVIDER;
+                /**
+                 * Available actions
+                 * NONE, - don't try to load ad
+                 * RETRY_SAME_PROVIDER, - try to load ad of the same provider
+                 * TRY_NEXT_PROVIDER - try to load ad of the next provider
+                 */
+            }
+
+            @Override
+            public void onFailLoadAdAllProviders() {
+                // Failed to load ad
+                if (attemptCount < MAX_ATTEMPT_COUNT) {
+                    attemptCount++;
+                    loadAds();
+                }
+            }
+
+            @Override
+            public boolean onShouldSkipRTBProviders() {
+                // return true if you want to skip ad from RTB provider
+                return false;
+            }
+        });
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.size() + ads.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // some magic logic for positioning ads items =)
+            if (ads.size() == 0) {
+                return items.get(position);
+            } else {
+                int adIndex = position / 10;
+                if (position % 10 == 0 && adIndex < ads.size()) {
+                    return ads.get(adIndex);
+                } else {
+                    int index = position - Math.min(adIndex, ads.size());
+                    return items.get(index);
+                }
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Object item = getItem(position);
+            if (item instanceof NativeAd) {
+                return getNativeAdView(convertView, (NativeAd)item, parent);
+            } else {
+                return getItemView(convertView, (String)item, parent);
+            }
+        }
+
+        View getNativeAdView(View convertView, NativeAd ad, ViewGroup parent) {
+            if (convertView == null || !(convertView.getTag() instanceof NativeAdViewHolder)) {
+                NativeAdViewHolder holder = new NativeAdViewHolder();
+                LinearLayout view = (LinearLayout)View.inflate(parent.getContext(), R.layout.native_ad_view, null);
+                holder.iconView = (ImageView)view.findViewById(R.id.icon);
+                holder.titleView = (TextView)view.findViewById(R.id.title);
+                holder.imageView = (ImageView)view.findViewById(R.id.image);
+                holder.descriptionView = (TextView)view.findViewById(R.id.text);
+                holder.ratingView = (TextView)view.findViewById(R.id.rating);
+                holder.clickButton = (Button)view.findViewById(R.id.installButton);
+                holder.fillView(ad, view);
+                view.setTag(holder);
+                return view;
+            } else {
+                NativeAdViewHolder holder = (NativeAdViewHolder)convertView.getTag();
+                holder.fillView(ad, convertView);
+                return convertView;
+            }
+        }
+
+        View getItemView(View convertView, String item, ViewGroup parent) {
+            if (convertView == null || !(convertView.getTag() instanceof ItemViewHolder)) {
+                ItemViewHolder holder = new ItemViewHolder();
+                LinearLayout view = (LinearLayout)View.inflate(parent.getContext(), R.layout.item_view, null);
+                holder.textView = (TextView)view.findViewById(R.id.text);
+                holder.textView.setText(item);
+                view.setTag(holder);
+                return view;
+            } else {
+                ItemViewHolder holder = (ItemViewHolder)convertView.getTag();
+                holder.textView.setText(item);
+                return convertView;
+            }
+        }
+    }
+
+    class ItemViewHolder {
+        TextView textView;
+    }
+
+    class NativeAdViewHolder {
+        ImageView iconView;
+        TextView titleView;
+        ImageView imageView;
+        TextView descriptionView;
+        TextView ratingView;
+        Button clickButton;
+
+        void fillView(final NativeAd ad, View view) {
+            titleView.setText(ad.getTitle());
+            descriptionView.setText(ad.getDescriptionText());
+            ImageLoader imageLoader = ImageLoader.getInstance();
+            imageLoader.displayImage(ad.getIconUrl(), iconView);
+            imageLoader.displayImage(ad.getImageUrl(), imageView);
+            ratingView.setText("Rating " + String.valueOf(ad.getStarRaiting()));
+            if (ad.getCtaText() != null) {
+                clickButton.setText(ad.getCtaText());
+            } else {
+                clickButton.setText("Install");
+            }
+            clickButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ad.displayAndHandleClick(MainActivity.this);
+                }
+            });
+            ad.detachFromView();
+            ad.trackImpression();
+            ad.attachToView(view);
+        }
+    }
+}
